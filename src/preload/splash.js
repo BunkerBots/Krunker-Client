f5de8c3eb7eca1644065c96f2f04a2f3abc7b1cb
EJ BEAN <danonymousarmy@gmail.com>
2021-05-28 00:56:34 +0530

    Electron builder commit

--- src/preload/splash.js
+++ src/preload/splash.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 const { ipcRenderer } = require('electron');
-
+const { version } = require('../data/core.json');
 const tips = [
     'Use F5 to reload your page',
-    'want to switch lobbies faster? hit F6'
+    'want to switch lobbies faster? hit F6',
+    'Hit F2 to copy the current game link to your clipboard'
 ];
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,11 +15,17 @@ window.addEventListener('DOMContentLoaded', () => {
      * @param {string} text
      * @returns element
      */
-    const viewTips = (selector, text) => {
+    const replaceText = (selector, text) => {
         const element = document.getElementById(selector);
-        if (element) element.innerHTML = text;
+        if (element) element.innerText = text;
         return element;
     };
     const tip = tips[Math.floor(Math.random() * tips.length)];
-    viewTips('tips', `Tips: ${tip}`);
+    replaceText('tips', `Tips: ${tip}`);
+    replaceText('version', `v${version}`);
+});
+
+ipcRenderer.on('message', text => {
+    const element = document.getElementById('status');
+    element.innerText = text;
 });
