8e4dcd3d7d73c67210678794cf305080b7469b6a
EJ BEAN <danonymousarmy@gmail.com>
2021-05-27 22:31:44 +0530

    fix: dir, splash screen & cleanup

--- src/main.js
+++ src/main.js
@@ -8,7 +8,32 @@ const krunkerurl = 'https://krunker.io/';
 const Store = require('electron-store');
 const store = new Store();
 const shortcuts = require('electron-localshortcut');
-const { evalURL } = require('./utils/evalURL');
+const { evalURL, autoUpdate } = require('./utils');
+// const { initClient } = require('./client/client');
+
+
+app.commandLine.appendSwitch('disable-frame-rate-limit');
+app.commandLine.appendSwitch('ignore-gpu-blacklist');
+app.commandLine.appendSwitch('disable-breakpad');
+app.commandLine.appendSwitch('disable-component-update');
+app.commandLine.appendSwitch('disable-print-preview');
+app.commandLine.appendSwitch('disable-metrics');
+app.commandLine.appendSwitch('disable-metrics-repo');
+app.commandLine.appendSwitch('enable-javascript-harmony');
+app.commandLine.appendSwitch('enable-future-v8-vm-features');
+app.commandLine.appendSwitch('enable-webgl2-compute-context');
+app.commandLine.appendSwitch('disable-hang-monitor');
+app.commandLine.appendSwitch('no-referrers');
+app.commandLine.appendSwitch('renderer-process-limit', 100);
+app.commandLine.appendSwitch('max-active-webgl-contexts', 100);
+app.commandLine.appendSwitch('enable-quic');
+app.commandLine.appendSwitch('high-dpi-support', 1);
+app.commandLine.appendSwitch('ignore-gpu-blacklist');
+app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
+app.commandLine.appendSwitch('disable-bundled-ppapi-flash');
+app.commandLine.appendSwitch('disable-logging');
+app.commandLine.appendSwitch('disable-web-security');
+app.commandLine.appendSwitch('webrtc-max-cpu-consumption-percentage=100');
 
 if (!app.requestSingleInstanceLock()) app.quit();
 
@@ -17,14 +42,19 @@ let splash = null;
 let social = null;
 // functions
 
+/**
+ * Keyboard shortcuts binder
+ * @returns game window object
+ */
+
 function createShortcutKeys() {
     const contents = game.webContents;
     const clipboardURL = clipboard.readText();
     const type = evalURL(clipboardURL);
-    contents.on('dom-ready', () => {
-        if (type == 'game') shortcuts.register(game, 'F3', () => createGameWindow(clipboardURL)); // load game from clipboard
-        else if (type == 'social') shortcuts.register(game, 'F3', () => createSocialWindow(clipboardURL));
-    });
+    // contents.on('dom-ready', () => {
+    //     if (type == 'game') shortcuts.register(game, 'F3', () => createGameWindow(clipboardURL)); // load game from clipboard
+    //     else if (type == 'social') shortcuts.register(game, 'F3', () => createSocialWindow(clipboardURL));
+    // });
     shortcuts.register(game, 'Escape', () => contents.executeJavaScript('document.exitPointerLock()', true));
     shortcuts.register(game, 'F5', () => contents.reload()); // reload page
     shortcuts.register(game, 'Shift+F5', () => contents.reloadIgnoringCache());
@@ -41,6 +71,11 @@ function createShortcutKeys() {
     return game;
 }
 
+/**
+ * Creates a splash window for the client
+ * @returns splash window (object)
+ */
+
 function createSplashWindow() {
     splash = new BrowserWindow({
         width: 600,
@@ -51,12 +86,20 @@ function createSplashWindow() {
         transparent: true,
         alwaysOnTop: true,
         webPreferences: {
-            preload: `${__dirname}/splash.js`,
+            preload: `${__dirname}/preload/splash.js`,
         }
     });
+    console.log(`${__dirname}/preload/splash.js`);
     splash.loadFile('src/html/splash.html');
     return splash;
 }
+
+/**
+ * Creates the main game window
+ * @param {string} url
+ * @param {*} webContents
+ * @returns game window object
+ */
 
 function createGameWindow(url, webContents) {
     const { width, height } = screen.getPrimaryDisplay().workArea;
@@ -67,18 +110,17 @@ function createGameWindow(url, webContents) {
         show: false,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
+            preload: `${__dirname}/preload/preload.js`,
             contextIsolation: false,
         }
     });
     const contents = game.webContents;
     createShortcutKeys();
-    // keys(game, createGameWindow, app, true);
     if (!webContents) game.loadURL(url);
     return game;
 }
 
-function createSocialWindow(url, webContents) {
+function createWindow(url, webContents) {
     const { width, height } = screen.getPrimaryDisplay().workArea;
     social = new BrowserWindow({
         width: width,
@@ -90,10 +132,13 @@ function createSocialWindow(url, webContents) {
             contextIsolation: false,
         }
     });
-    // keys(game, createGameWindow, app, true);
     if (!webContents) social.loadURL(url);
     return social;
 }
+
+/**
+ * Main function, splash window + game window
+ */
 
 function initClient() { // splash and game
     const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -104,6 +149,10 @@ function initClient() { // splash and game
             splashScreen.destroy();
             gameScreen.show();
         });
+        // autoUpdate(splash.webContents, splash).finally(() => {
+        //     splash.destroy();
+        //     gameScreen.show();
+        // });
     });
 }
 
